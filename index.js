8f721296e25c50b1777d2c99f22cd5ef1ded9f6c
Daniel <dlevett@hotmail.co.uk>
2017-05-07 20:09:48 +0100

    add findClosest function and export via index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+import { findClosest, createFindClosest } from './lib/findClosest';
+import { findClosestIndex, createFindClosestIndex } from './lib/findClosestIndex';
+
+export default findClosest;
+export {
+	findClosest,
+	findClosestIndex,
+	createFindClosest,
+	createFindClosestIndex
+};

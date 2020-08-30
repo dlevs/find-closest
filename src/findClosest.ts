290a58409de69fe2510a51f03a4218be1ae0ffe2
Daniel Levett <dlevett@hotmail.co.uk>
2020-08-30 13:01:33 +0100

    Fix filterMapFn needing to be explicitly passed as "undefined"

--- src/findClosest.ts
+++ src/findClosest.ts
@@ -1,83 +1,85 @@
-type IterationFn<T, R> = (value: T, index: number, array: T[]) => R;
-
-type FilterMapFn<T> = [T] extends [number]
-	// Optional map function. Can return `true`.
-	? IterationFn<T, number | boolean> | undefined
-	// Required map function. Cannot return `true`.
-	: IterationFn<T, number | false>;
-
-type FindFn = <T>(
-	haystack: T[],
-	needle: number,
-	filterMapFn: FilterMapFn<T>,
-) => T;
-
-type FindIndexFn = <T>(
-	haystack: T[],
-	needle: number,
-	filterMapFn: FilterMapFn<T>,
-) => number;
-
 /**
  * Returns the index of the item in an array that is closest to the value
  * specified by the `needle` argument.
  */
-export const findClosestIndex: FindIndexFn = (
-	haystack,
-	needle,
-	filterMapFn,
+export const findClosestIndex: Finder<number> = (
+  haystack: any[],
+  needle: number,
+  filterMapFn?: IterationFn<number, number | boolean>
 ) => {
-	let closest = {
-		index: -1,
-		distance: Number.POSITIVE_INFINITY,
-		value: 0,
-	};
+  let closest = {
+    index: -1,
+    distance: Number.POSITIVE_INFINITY,
+    value: 0,
+  }
 
-	for (let index = 0; index < haystack.length; index++) {
-		const rawValue = haystack[index];
-		let value: number;
+  for (let index = 0; index < haystack.length; index++) {
+    const rawValue = haystack[index]
+    let value: number
 
-		if (filterMapFn) {
-			const mappedValue = filterMapFn(rawValue, index, haystack);
+    if (filterMapFn) {
+      const mappedValue = filterMapFn(rawValue, index, haystack)
 
-			switch (mappedValue) {
-				case false:
-					continue;
-				case true:
-					if (typeof rawValue !== 'number') {
-						throw new TypeError(`\`filterMapFn\` returned \`true\` for non-number value \`${rawValue}\`.`);
-					}
-					value = rawValue;
-					break;
-				default:
-					value = mappedValue;
-			}
-		} else {
-			if (typeof rawValue !== 'number') {
-				throw new TypeError(`Array contains non-number value \`${rawValue}\` without a \`filterMapFn\` to map it to a number.`);
-			}
-			value = rawValue;
-		}
+      switch (mappedValue) {
+        case false:
+          continue
+        case true:
+          if (typeof rawValue !== 'number') {
+            throw new TypeError(
+              `\`filterMapFn\` returned \`true\` for non-number value \`${rawValue}\`.`
+            )
+          }
+          value = rawValue
+          break
+        default:
+          value = mappedValue
+      }
+    } else {
+      if (typeof rawValue !== 'number') {
+        throw new TypeError(
+          `Array contains non-number value \`${rawValue}\` without a \`filterMapFn\` to map it to a number.`
+        )
+      }
+      value = rawValue
+    }
 
-		const distance = Math.abs(value - needle);
+    const distance = Math.abs(value - needle)
 
-		if (distance === 0) {
-			return index;
-		}
+    if (distance === 0) {
+      return index
+    }
 
-		if (distance < closest.distance) {
-			closest = { index, distance, value };
-		}
-	}
+    if (distance < closest.distance) {
+      closest = { index, distance, value }
+    }
+  }
 
-	return closest.index;
-};
+  return closest.index
+}
 
 /**
  * Returns the item in an array that is closest to the value specified by the
  * `needle` argument.
  */
-export const findClosest: FindFn = (haystack, needle, filterMapFn) =>
-	haystack[findClosestIndex(haystack, needle, filterMapFn)];
+export const findClosest: Finder = (
+  haystack: any[],
+  needle: number,
+  filterMapFn?: IterationFn<number, number | boolean>
+) => haystack[findClosestIndex(haystack, needle, filterMapFn)]
 
-export default findClosest;
+export default findClosest
+
+interface Finder<R = unknown> {
+  (
+    haystack: number[],
+    needle: number,
+    filterMapFn?: IterationFn<number, number | boolean>
+  ): number
+  <T>(
+    haystack: T[],
+    needle: number,
+    filterMapFn: IterationFn<T, number | false>
+  ): R extends unknown ? T : number
+}
+
+type IterationFn<T, R> = (value: T, index: number, array: T[]) => R
